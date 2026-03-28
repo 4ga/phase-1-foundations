47bef487aa4ded8521408f1188a5544eba856025
Grace A <gakpgit@gmail.com>
2026-03-27 20:25:37 -0600

    Add first JavaScript variables practice

---
+++ javascript-fundamentals/index.js
@@ -0,0 +1,6 @@
+const firstName = "Grace";
+const favoriteNumber = 7;
+
+console.log(firstName);
+console.log(favoriteNumber);
+console.log("My name is " + firstName + " and my favorite number is " + favoriteNumber);	
